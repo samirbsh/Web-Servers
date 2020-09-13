67326a44b15071035aa42dc888ef5647af8fe420
samirbsh <samirkumar2527@gmail.com>
2020-09-14 00:45:55 +0530

    Advance Templating

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
+
 // Defined paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "./templates");
+const viewsPath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname,"./templates/partials")
 // Setup handlebars engine and views Location
 // set allows to set a value
 app.set("view engine", "hbs");
 // If there is change in the name of the view folder
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
 // Set up static directory to serve 
 // Serving(customizing) up the server
 app.use(express.static(publicDirectoryPath));
@@ -19,19 +24,21 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather App",
-    name: "Samir Kumar",
+    name: "Samir",
   });
 });
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Samir Kumar",
+    name: "Ajay",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is help text",
+    title: 'help',
+    name: "Samir Kumar",
   });
 });
 
@@ -48,3 +55,4 @@ app.listen(3000, () => {
 // example: If we want to provide a header and footer in each page then we
 // writing the same in every page is not ideal instead it would be ideal
 // if we define it at some single page.
+// Partials : Allows to create template which is a part of a web page.(Showing same thing in different pages)
