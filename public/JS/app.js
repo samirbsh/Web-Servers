95dc45e78e5a05fbe5643725fb9c3ce828026329
samirbsh <samirkumar2527@gmail.com>
2020-09-13 23:49:51 +0530

    Dynamic Pages with Templating

--- public/JS/app.js
+++
@@ -1,1 +0,0 @@
-console.log("client side Javascript file is loaded.")
--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
 // Serving(customizing) up the server
 app.use(express.static(publicDirectoryPath))
+
+// set allows to set a value
+app.set('view engine', 'hbs')
 // Routes
+// Dynamic routing
+// First argument is  the name to render and second argument is the object that view will be accessing.
+app.get('',(req,res) => {
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Samir Kumar'
+    })
+})
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title: 'About Me',
+        name: 'Samir Kumar'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpText:'This is help text'
+    })
+})
 
 app.get('/weather',(req,res) =>{
     res.send('Weather')
@@ -14,3 +37,9 @@ app.get('/weather',(req,res) =>{
 app.listen(3000,()=>{
     console.log('Server is listening on port 3000.')
 })
+
+// To render dynamic web pages we need template engine.
+// here the template engine will be handle bars
+// example: If we want to provide a header and footer in each page then we
+// writing the same in every page is not ideal instead it would be ideal
+// if we define it at some single page.
